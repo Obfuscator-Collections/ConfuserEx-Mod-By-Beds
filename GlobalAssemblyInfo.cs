b414a43156255d3e2823fbe627d8616feddf11ae
VNGhostMans <thanh20057a@gmail.com>
2022-02-23 23:15:41 +0700

    first commit

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("Beds Protector")]
+[assembly: AssemblyCompany("Bed")]
+[assembly: AssemblyCopyright("Copyright (C) K019")]
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+#else
+
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+
+[assembly: AssemblyVersion("1.4.1")]
+[assembly: AssemblyFileVersion("1.4.1")]
+[assembly: AssemblyInformationalVersion("v1.4.1 | Public Version @Github")]
